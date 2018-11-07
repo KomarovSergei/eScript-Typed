299776ee7eb85b56027eec311f7a0040e4485776
unknown <Sergey@PS.local>
2018-11-07 21:30:55 +0300

    add fix for BC type def

--- test_types/normal_script2.ts
+++ test_types/normal_script2.ts
@@ -1,38 +1,31 @@
-function LookupName(sTypeName:String,sValName:String):String
-{
-	var sResult:chars = "";
-	try
-	{
-		var boLOV:BusObject = TheApplication().GetBusObject("List Of Values");
-		var bcLOV:BusComp = boLOV.GetBusComp("List Of Values");
-		var isRecord:bool = false;
-		var sSearchExpr:chars = "[Type] = '" + sTypeName + "' and [Value] = '"+ sValName +"'";
+function LookupName(sTypeName: String, sValName: String): String {
+	var sResult: chars = "";
+	try {
+		var boLOV: BusObject = TheApplication().GetBusObject("List Of Values");
+		var bcLOV: BusComp = boLOV.GetBusComp("List Of Values");
+		var isRecord: bool = false;
+		var sSearchExpr: chars = "[Type] = '" + sTypeName + "' and [Value] = '" + sValName + "'";
 
-		with (bcLOV)
-		{
+		with(bcLOV) {
 			SetViewMode(AllView);
 			ActivateField("Type");
 			ClearToQuery();
 			SetSearchExpr(sSearchExpr);
 			ExecuteQuery(ForwardOnly);
 			isRecord = FirstRecord();
-			if (isRecord)
-			{
+			if (isRecord) {
 				sResult = GetFieldValue("Name");
 			}
 		}
-	}
-	catch(e)
-	{
+	} catch (e) {
 		TheApplication().RaiseErrorText(e.errText);
-	}
-	finally
-	{
+	} finally {
 		bcLOV = null;
 		boLOV = null;
 	}
 	return sResult;
 }
+
 function UpdateAccount(Inputs: PropertySet, Outputs: PropertySet) {
 	var sImpId: chars = Inputs.GetProperty("ImportId");
 	if (sImpId == null || sImpId == "") {
@@ -47,7 +40,7 @@ function UpdateAccount(Inputs: PropertySet, Outputs: PropertySet) {
 		var inTransProp: PropertySet = TheApplication().NewPropertySet();
 		var outTransProp: PropertySet = TheApplication().NewPropertySet();
 		var TransCheck;
-		var bcImpCnt:BusComp = TheApplication().GetBusObject("SBRF Import Center").GetBusComp("SBRF Import Center");
+		var bcImpCnt: BusComp = TheApplication().GetBusObject("SBRF Import Center").GetBusComp("SBRF Import Center");
 		var nRecordCount = 0;
 		var SuccessRows = 0;
 		var ErrorRows = 0;
@@ -62,7 +55,7 @@ function UpdateAccount(Inputs: PropertySet, Outputs: PropertySet) {
 		var sOGRNDate;
 		var sAddr: String = "";
 		var sOPF: chars = "";
-		var sIndustry	: chars = "";
+		var sIndustry: chars = "";
 		var sKindActivity: chars = "";
 		var sMSP: chars = "";
 		var sFullName: chars = "";
@@ -80,11 +73,11 @@ function UpdateAccount(Inputs: PropertySet, Outputs: PropertySet) {
 		bcImpCnt.ActivateField("All Rows");
 		bcImpCnt.ActivateField("Error Rows");
 		bcImpCnt.ActivateField("Success Rows");
-		bcImpCnt.SetSearchExpr("[Id]='"+sImpId+"'");
+		bcImpCnt.SetSearchExpr("[Id]='" + sImpId + "'");
 		bcImpCnt.ExecuteQuery(ForwardOnly);
 		if (bcImpCnt.FirstRecord()) {
-			bcImpCnt.SetFieldValue("Start Date", (dtDate.getMonth()+1)+"/"+dtDate.getDate()+"/"+dtDate.getFullYear()+" "+dtDate.getHours()+":"+dtDate.getMinutes()+":"+dtDate.getSeconds());
-		
+			bcImpCnt.SetFieldValue("Start Date", (dtDate.getMonth() + 1) + "/" + dtDate.getDate() + "/" + dtDate.getFullYear() + " " + dtDate.getHours() + ":" + dtDate.getMinutes() + ":" + dtDate.getSeconds());
+
 			bcImpAcc.ClearToQuery();
 			bcImpAcc.ActivateField("Account Id");
 			bcImpAcc.ActivateField("Name");
@@ -138,7 +131,7 @@ function UpdateAccount(Inputs: PropertySet, Outputs: PropertySet) {
 				sOKK = bcImpAcc.GetFieldValue("OKK");
 				sSubIndustry = bcImpAcc.GetFieldValue("Sub Industry");
 				sMacroIndustry = bcImpAcc.GetFieldValue("Macro Industry");
-				
+
 				try {
 					bcAcc.ClearToQuery();
 					bcAcc.InvokeMethod("SetAdminMode", "TRUE");
@@ -166,27 +159,23 @@ function UpdateAccount(Inputs: PropertySet, Outputs: PropertySet) {
 
 					if (sAccId == null || sAccId == "") {
 						if (sINN != "" && sINN != null) {
-							
-							bcAcc.SetSearchExpr("[SBRF INN] = '"+ sINN +"' AND ([Type] = '"+TheApplication().InvokeMethod("LookupValue", "ACCOUNT_TYPE", "Account")+"' OR [Type] = '"+TheApplication().InvokeMethod("LookupValue", "ACCOUNT_TYPE", "Individual")+"')");//##SHABAROV 23082018 CRP-49602
+
+							bcAcc.SetSearchExpr("[SBRF INN] = '" + sINN + "' AND ([Type] = '" + TheApplication().InvokeMethod("LookupValue", "ACCOUNT_TYPE", "Account") + "' OR [Type] = '" + TheApplication().InvokeMethod("LookupValue", "ACCOUNT_TYPE", "Individual") + "')"); //##SHABAROV 23082018 CRP-49602
 							bcAcc.ExecuteQuery(ForwardOnly);
 							if (bcAcc.FirstRecord()) {
-								
-								sAccId = bcAcc.GetFieldValue("Id");//SHABAROV 24082018 CRP-49228
+
+								sAccId = bcAcc.GetFieldValue("Id"); //SHABAROV 24082018 CRP-49228
 								UpsertAccount(bcAcc, sName, sKPP, sOGRN, sOGRNDate, sOPF, sIndustry, sKindActivity, sMSP, sFullName, sOKPO, sOKVED, sPrimaryOKVED, sOKATO, sOKK, sSubIndustry, sMacroIndustry);
 								if (sAddr != "" && sAddr != null) {
 									UpsertAddress(bcImpAcc, sAccId, sAddr);
 								}
-							}
-							else{
+							} else {
 								TheApplication().RaiseErrorText("Организация с таким ИНН не найдена.");
 							}
-						}
-						else{
+						} else {
 							TheApplication().RaiseErrorText("Не указан ни СRMId ни ИНН организации для поиска.");
 						}
-					}
-					else
-					{
+					} else {
 						bcAcc.SetSearchSpec("Id", sAccId);
 						bcAcc.ExecuteQuery(ForwardOnly);
 
@@ -198,9 +187,7 @@ function UpdateAccount(Inputs: PropertySet, Outputs: PropertySet) {
 							if (sAddr != "" && sAddr != null) {
 								UpsertAddress(bcImpAcc, sAccId, sAddr);
 							}
-						}
-						else
-						{
+						} else {
 							TheApplication().RaiseErrorText("Организация с таким Id не найдена.");
 						}
 					}
@@ -222,7 +209,7 @@ function UpdateAccount(Inputs: PropertySet, Outputs: PropertySet) {
 						Transact.InvokeMethod("EndTransaction", inTransProp, outTransProp);
 					}
 					bcImpAcc.SetFieldValue("Status", "Ошибка");
-					bcImpAcc.SetFieldValue("Descr", e.message.substr(0,254));
+					bcImpAcc.SetFieldValue("Descr", e.message.substr(0, 254));
 					bcImpAcc.SetFieldValue("Error", e.errCode);
 					bcImpAcc.WriteRecord();
 					ErrorRows++;
@@ -230,7 +217,7 @@ function UpdateAccount(Inputs: PropertySet, Outputs: PropertySet) {
 				bImpRec = bcImpAcc.NextRecord();
 			}
 			dtDate = new Date();
-			bcImpCnt.SetFieldValue("End Date",(dtDate.getMonth()+1)+"/"+dtDate.getDate()+"/"+dtDate.getFullYear()+" "+dtDate.getHours()+":"+dtDate.getMinutes()+":"+dtDate.getSeconds());
+			bcImpCnt.SetFieldValue("End Date", (dtDate.getMonth() + 1) + "/" + dtDate.getDate() + "/" + dtDate.getFullYear() + " " + dtDate.getHours() + ":" + dtDate.getMinutes() + ":" + dtDate.getSeconds());
 			bcImpCnt.SetFieldValue("All Rows", nRecordCount);
 			bcImpCnt.SetFieldValue("Error Rows", ErrorRows);
 			bcImpCnt.SetFieldValue("Success Rows", SuccessRows);
@@ -269,10 +256,9 @@ function UpdateAccount(Inputs: PropertySet, Outputs: PropertySet) {
 		sImpId = null;
 	}
 }
-function UpsertAccount(bcAcc:BusComp, sName:chars, sKPP:chars, sOGRN:chars, sOGRNDate:chars, sOPF:chars, sIndustry:chars, sKindActivity:chars, sMSP:chars, sFullName :chars, sOKPO :chars, sOKVED :chars, sPrimaryOKVED:chars, sOKATO :chars, sOKK :chars, sSubIndustry :chars, sMacroIndustry :chars)
-{
-	try
-	{
+
+function UpsertAccount(bcAcc: BusComp, sName: chars, sKPP: chars, sOGRN: chars, sOGRNDate: chars, sOPF: chars, sIndustry: chars, sKindActivity: chars, sMSP: chars, sFullName: chars, sOKPO: chars, sOKVED: chars, sPrimaryOKVED: chars, sOKATO: chars, sOKK: chars, sSubIndustry: chars, sMacroIndustry: chars) {
+	try {
 		var boImp: BusObject = bcAcc.BusObject();
 		var bcMVGIndustry: BusComp = bcAcc.GetMVGBusComp("Industry");
 		var assocBC = bcMVGIndustry.GetAssocBusComp();
@@ -283,7 +269,7 @@ function UpsertAccount(bcAcc:BusComp, sName:chars, sKPP:chars, sOGRN:chars, sOGR
 
 		if (sName != "" && sName != null) {
 			bcAcc.SetFieldValue("Name", sName);
-			}
+		}
 		if (sKPP != "" && sKPP != null) {
 			bcAcc.SetFieldValue("SBRF KPP", sKPP);
 		}
@@ -296,33 +282,28 @@ function UpsertAccount(bcAcc:BusComp, sName:chars, sKPP:chars, sOGRN:chars, sOGR
 		if (sOPF != "" && sOPF != null) {
 			if (LookupName("SBRF_OPF", sOPF) == "") {
 				TheApplication().RaiseErrorText("Некорректный ОПФ");
-			}
-			else{
+			} else {
 				//bcAcc.SetFieldValue("SBRF OPF", LookupName("SBRF_OPF", sOPF));
 				bcAcc.SetFieldValue("SBRF OPF", sOPF);
 			}
 		}
 		if (sIndustry != "" && sIndustry != null) {
-			if (sKindActivity != "" && sKindActivity != null){
+			if (sKindActivity != "" && sKindActivity != null) {
 				if (LookupName("SBRF_INDUSTRY", sIndustry) == "") {
 					TheApplication().RaiseErrorText("Некорректное значение отрасли");
-				}
-				else
-				{
+				} else {
 					bcKindActivity.ClearToQuery();
 					bcKindActivity.InvokeMethod("SetAdminMode", "TRUE");
 					bcKindActivity.SetViewMode(AllView);
 					bcKindActivity.ActivateField("SBRF Branch");
 					bcKindActivity.ActivateField("SBRF Kind of Activity");
-					bcKindActivity.SetSearchExpr("[SBRF Branch]='"+sIndustry+"' AND [SBRF Kind of Activity]='" +sKindActivity+ "'");
+					bcKindActivity.SetSearchExpr("[SBRF Branch]='" + sIndustry + "' AND [SBRF Kind of Activity]='" + sKindActivity + "'");
 					bcKindActivity.ExecuteQuery(ForwardOnly);
-					if(bcKindActivity.FirstRecord()){
+					if (bcKindActivity.FirstRecord()) {
 						bcAcc.SetFieldValue("SBRF Industry", sIndustry);
 						//bcAcc.WriteRecord();
 						bcAcc.SetFieldValue("SBRF Kind Activity", sKindActivity);
-					}
-					else
-					{
+					} else {
 						TheApplication().RaiseErrorText("Некорректное значение отрасли");
 					}
 				}
@@ -339,22 +320,21 @@ function UpsertAccount(bcAcc:BusComp, sName:chars, sKPP:chars, sOGRN:chars, sOGR
 		}
 		if (sOKVED != "" && sOKVED != null) {
 			bcMVGIndustry.ActivateField("SIC Code");
-			bcMVGIndustry.SetSearchExpr("[SIC Code]='"+sOKVED+"'");
+			bcMVGIndustry.SetSearchExpr("[SIC Code]='" + sOKVED + "'");
 			bcMVGIndustry.ExecuteQuery(ForwardOnly);
-			if(!bcMVGIndustry.FirstRecord()){
+			if (!bcMVGIndustry.FirstRecord()) {
 				assocBC.ActivateField("SIC Code");
-				assocBC.SetSearchExpr("[SIC Code]='"+sOKVED+"'");
+				assocBC.SetSearchExpr("[SIC Code]='" + sOKVED + "'");
 				assocBC.ExecuteQuery(ForwardOnly);
-					if(assocBC.FirstRecord()){
-						assocBC.Associate(NewAfter);
-						if (sPrimaryOKVED == "Y"){
-							bcAcc.SetFieldValue("Primary Industry Id", assocBC.GetFieldValue("Id"));	
-						}
-						
+				if (assocBC.FirstRecord()) {
+					assocBC.Associate(NewAfter);
+					if (sPrimaryOKVED == "Y") {
+						bcAcc.SetFieldValue("Primary Industry Id", assocBC.GetFieldValue("Id"));
 					}
-					else{
-						TheApplication().RaiseErrorText("Некорректный ОКВЭД");
-					}
+
+				} else {
+					TheApplication().RaiseErrorText("Некорректный ОКВЭД");
+				}
 			}
 		}
 		if (sOKATO != "" && sOKATO != null) {
@@ -365,10 +345,9 @@ function UpsertAccount(bcAcc:BusComp, sName:chars, sKPP:chars, sOGRN:chars, sOGR
 			bcClassifyTypes.ActivateField("SBRF Value Code");
 			bcClassifyTypes.SetSearchExpr("[SBRF Value Code]='" + sOKATO + "' AND [SBRF Classify Type LIC]='OKATO'");
 			bcClassifyTypes.ExecuteQuery(ForwardOnly);
-			if(bcClassifyTypes.FirstRecord()){
-				bcAcc.SetFieldValue("SBRF OKATO Code", sOKATO);	
-			}
-			else{
+			if (bcClassifyTypes.FirstRecord()) {
+				bcAcc.SetFieldValue("SBRF OKATO Code", sOKATO);
+			} else {
 				TheApplication().RaiseErrorText("Некорректный ОКАТО");
 			}
 		}
@@ -380,35 +359,29 @@ function UpsertAccount(bcAcc:BusComp, sName:chars, sKPP:chars, sOGRN:chars, sOGR
 			bcOKK.ActivateField("Code Ext");
 			bcOKK.SetSearchExpr("[Code Ext]='" + sOKK + "'");
 			bcOKK.ExecuteQuery(ForwardOnly);
-			if(bcOKK.FirstRecord()){
-				bcOKK.Pick();	
-			}
-			else{
+			if (bcOKK.FirstRecord()) {
+				bcOKK.Pick();
+			} else {
 				TheApplication().RaiseErrorText("Некорректный ОКК");
 			}
-		}
-
-		else{
+		} else {
 			if (sSubIndustry != "" && sSubIndustry != null) {
 				if (sMacroIndustry != "" && sMacroIndustry != null) {
 					bcOKK.ClearToQuery();
 					bcOKK.InvokeMethod("SetAdminMode", "TRUE");
 					bcOKK.SetViewMode(AllView);
 					bcOKK.ActivateField("Value");
-					bcOKK.SetSearchExpr("[Value]='"+sSubIndustry+"' AND [Parent Value]='" +sMacroIndustry+ "'");
+					bcOKK.SetSearchExpr("[Value]='" + sSubIndustry + "' AND [Parent Value]='" + sMacroIndustry + "'");
 					bcOKK.ExecuteQuery(ForwardOnly);
-					if(bcOKK.FirstRecord()){
-						bcOKK.Pick();	
-					}
-					else{
+					if (bcOKK.FirstRecord()) {
+						bcOKK.Pick();
+					} else {
 						TheApplication().RaiseErrorText("Значение Подоотрасли не соответствует Макроотрасли клиента");
 					}
-				}
-				else{
+				} else {
 					TheApplication().RaiseErrorText("Значение Подоотрасли не соответствует Макроотрасли клиента");
 				}
-			}
-			else{
+			} else {
 				if (sMacroIndustry != "" && sMacroIndustry != null) {
 					bcMacro.ClearToQuery();
 					bcMacro.InvokeMethod("SetAdminMode", "TRUE");
@@ -416,22 +389,17 @@ function UpsertAccount(bcAcc:BusComp, sName:chars, sKPP:chars, sOGRN:chars, sOGR
 					bcMacro.ActivateField("Value");
 					bcMacro.SetSearchExpr("[Value]='" + sMacroIndustry + "'");
 					bcMacro.ExecuteQuery(ForwardOnly);
-					if(bcMacro.FirstRecord()){
-						bcMacro.Pick();	
-					}
-					else{
+					if (bcMacro.FirstRecord()) {
+						bcMacro.Pick();
+					} else {
 						TheApplication().RaiseErrorText("Некорректная Макроотрасль");
 					}
 				}
 			}
 		}
-	}
-	catch(e)
-	{
+	} catch (e) {
 		TheApplication().RaiseErrorText(e.message);
-	}
-	finally
-	{
+	} finally {
 		bcKindActivity = null;
 		bcMVGIndustry = null;
 		assocBC = null;
@@ -439,69 +407,60 @@ function UpsertAccount(bcAcc:BusComp, sName:chars, sKPP:chars, sOGRN:chars, sOGR
 		boImp = null;
 	}
 }
-function UpsertAddress(bcImpAcc:BusComp, sAccId:String, sAddr:String)
-{
 
-	try
-	{
+function UpsertAddress(bcImpAcc: BusComp, sAccId: String, sAddr: String) {
+
+	try {
 		var boImp: BusObject = bcImpAcc.BusObject();
 		var bcAccount: BusComp = boImp.GetBusComp("SBRF UCP Account Lite");
-				
+
 		bcAccount.ClearToQuery();
-		bcAccount.InvokeMethod("SetAdminMode","TRUE");
+		bcAccount.InvokeMethod("SetAdminMode", "TRUE");
 		bcAccount.SetViewMode(AllView);
 		bcAccount.ActivateField("Primary Address Id");
 		bcAccount.SetSearchExpr("[Id] = '" + sAccId + "'");
 		bcAccount.ExecuteQuery(ForwardOnly);
-		if(bcAccount.FirstRecord())
-		{
+		if (bcAccount.FirstRecord()) {
 			//смотрим существует ли под организацией адрес с типом Юридический
 			var bcMVGAddr: BusComp = bcAccount.GetMVGBusComp("Street Address");
 			bcMVGAddr.ClearToQuery();
-			bcMVGAddr.InvokeMethod("SetAdminMode","TRUE");
+			bcMVGAddr.InvokeMethod("SetAdminMode", "TRUE");
 			bcMVGAddr.SetViewMode(AllView);
 			bcMVGAddr.ActivateField("Account Id");
 			bcMVGAddr.ActivateField("Street Address");
 			bcMVGAddr.ActivateField("Type");
-			bcMVGAddr.SetSearchExpr("[Account Id]='"+sAccId+"' AND [Type]='Юридический'");
+			bcMVGAddr.SetSearchExpr("[Account Id]='" + sAccId + "' AND [Type]='Юридический'");
 			bcMVGAddr.ExecuteQuery(ForwardOnly);
 			//если да - апдейтим
-			if(bcMVGAddr.FirstRecord())
-			{
-				bcMVGAddr.SetFieldValue("Street Address",sAddr);
+			if (bcMVGAddr.FirstRecord()) {
+				bcMVGAddr.SetFieldValue("Street Address", sAddr);
 				bcMVGAddr.WriteRecord();
 			}
 			//если нет - создаем
-			else
-			{
+			else {
 				bcMVGAddr.NewRecord("NewAfter");
 				bcMVGAddr.SetFieldValue("Street Address", sAddr);
-				bcMVGAddr.SetFieldValue("Type",TheApplication().InvokeMethod("LookupValue","ADDR_TYPE","Legal Address"));
+				bcMVGAddr.SetFieldValue("Type", TheApplication().InvokeMethod("LookupValue", "ADDR_TYPE", "Legal Address"));
 				bcMVGAddr.WriteRecord();
 			}
-			bcMVGAddr.InvokeMethod("SetAdminMode","FALSE");
-			bcAccount.SetFieldValue("Primary Address Id",bcMVGAddr.GetFieldValue("Id"));
-			bcAccount.InvokeMethod("SetAdminMode","FALSE");
+			bcMVGAddr.InvokeMethod("SetAdminMode", "FALSE");
+			bcAccount.SetFieldValue("Primary Address Id", bcMVGAddr.GetFieldValue("Id"));
+			bcAccount.InvokeMethod("SetAdminMode", "FALSE");
 		}
-	}
-	catch(e)
-	{
+	} catch (e) {
 		TheApplication().RaiseErrorText(e.message);
-	}
-	finally
-	{
+	} finally {
 		bcAccount = null;
 		bcMVGAddr = null;
 		boImp = null;
 		sAccId = null;
 		sAddr = null;
-        bcImpAcc = null;
+		bcImpAcc = null;
 	}
 }
-function ValidateAccountData(bcImpAcc:BusComp, sAccId:String)
-{
-	try
-	{
+
+function ValidateAccountData(bcImpAcc: BusComp, sAccId: String) {
+	try {
 		var boImp: BusObject = bcImpAcc.BusObject();
 		var bcAccount: BusComp = boImp.GetBusComp("SBRF UCP Account Lite");
 
@@ -509,48 +468,41 @@ function ValidateAccountData(bcImpAcc:BusComp, sAccId:String)
 		var oInDvmPS = TheApplication().NewPropertySet();
 		var oOutDvmPS = TheApplication().NewPropertySet();
 
-		var sError:String = "";
-				
+		var sError: String = "";
+
 		bcAccount.ClearToQuery();
 		bcAccount.SetViewMode(AllView);
 		bcAccount.SetSearchExpr("[Id] = '" + sAccId + "'");
 		bcAccount.ExecuteQuery(ForwardOnly);
-		if(bcAccount.FirstRecord())
-		{
+		if (bcAccount.FirstRecord()) {
 			oInDvmPS.SetProperty("Active Object", "N");
 			oInDvmPS.SetProperty("BusObj", boImp);
 			oInDvmPS.SetProperty("BusComp", bcAccount);
 			oInDvmPS.SetProperty("Object Id", bcAccount.GetFieldValue("Id"));
 			oInDvmPS.SetProperty("Enable Log", "N");
 			oInDvmPS.SetProperty("Rule Set Name", "SBRF Account Validation CI");
-			oDvmBS.InvokeMethod("Validate", oInDvmPS, oOutDvmPS); 
+			oDvmBS.InvokeMethod("Validate", oInDvmPS, oOutDvmPS);
 			sError = oOutDvmPS.GetProperty("Return Message");
-			
-			if(sError > "")
-			{
+
+			if (sError > "") {
 				throw sError;
 			}
 		}
-	}
-	catch(e)
-	{
+	} catch (e) {
 		TheApplication().RaiseErrorText(e.message);
-	}
-	finally
-	{
+	} finally {
 		bcAccount = null;
 		sError = null;
 		oOutDvmPS = null;
 		oInDvmPS = null;
 		oDvmBS = null;
-		sAccId  = null;
-        boImp = null;
+		sAccId = null;
+		boImp = null;
 	}
 }
-function Service_PreInvokeMethod (MethodName, Inputs, Outputs)
-{
-	if (MethodName == "UpdateAccount")
-	{
+
+function Service_PreInvokeMethod(MethodName, Inputs, Outputs) {
+	if (MethodName == "UpdateAccount") {
 		UpdateAccount(Inputs, Outputs);
 		return (CancelOperation);
 	}
